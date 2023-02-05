1bf612fddee6ce71f2ac9b1600e5f4988641f827
pdapsuke <106060175+pdapsuke@users.noreply.github.com>
2023-02-05 21:12:11 +0900

    Add files via upload

---
+++ nodejs-codebuild-test.js
@@ -0,0 +1,12 @@
+module.exports = function fizzbuzz (num) {
+    if (num % 15 === 0) {
+      return 'FizzBuzz'
+    }
+    if (num % 3 === 0) {
+      return 'Fizz'
+    }
+    if (num % 5 === 0) {
+      return 'Buzz'
+    }
+    return String(num)
+  }
